f35e663f96b498f0fa9f718c3b3ae53b971d9da2
HokyGUAN <hoky.guan@tymphany.com>
2021-01-27 10:33:50 +0800

    Update base db Time: 01.27
    Python Tool:
    Separate vinyl color from else

--- led_db_manager.py
+++ led_db_manager.py
@@ -8,18 +8,21 @@ class LED_DB_Manager():
 		self.int_datatype = 0
 		self.color_datatype = 0
 		self.total = 0
-		self.color_list = ["#2ccdff #5aa8eb #8486d8 #aa68c7 #ce4bb7 #eb34aa #fe25a1",
+		self.color_list = ["#2ccdff #5aa8eb #8486d8 #aa68c7 #ce4bb7 #eb34aa #ff24a1",
 							"#0064e6 #007be4 #008fe1 #00a2df #00b2de #00c0dc #00cadb",
 							"#ffe600 #ffbb00 #ff9100 #ff7c00 #ff6a00 #ff5e00 #ff5e00",
 							"#ff009c #ff299a #ff5297 #ff6c9e #ff78ac #ff84ba #ff91ca",
-							"#1fe6c3 #18cfb7 #12b8ab #0ca4a1 #079297 #03828f #00788a",
-							"#ffff00 #ff9766 #ff6596 #ac8cb8 #58b2db #00dbff #00dbff",
-							#"#1fe6c3 #6ce59b #b1e577 #f9e452 #a7c065 #559d77 #00788a",
-							#"#edc46a #f7a7a0 #fd97bf #ff91ca #fd97bf #f7a7a0 #edc46a",
-							#"#00cadb #8faad1 #e397cc #ff91ca #e397cc #8faad1 #00cadb",
-							#"#d8c93a #f29184 #a795b8 #00d0de #a8a25a #f08648 #d17db5",
+							"#006473 #057980 #0a8f8d #0fa59b #14baa8 #1ad0b5 #1fe6c3",
+							"#ffff00 #ffff00 #ff9d3c #ff3b79 #ad6fa4 #58a4d1 #00dbff",
 							"#15e8ff #2fe2e4 #ff9b23 #ff703f #ff4b57 #ff266f #ff0087",]
 
+		self.vinyl_color_list = ["#2ccdff #9578d0 #ff24a1 #000000 #000000",
+								"#0064e6 #0097e0 #00cadb #000000 #000000",
+								"#ffe600 #ffa200 #ff5e00 #000000 #000000",
+								"#ff009c #ff6596 #ff91ca #000000 #000000",
+								"#006473 #0fa59b #1fe6c3 #000000 #000000",
+								"#ffff00 #ff3b79 #00dbff #000000 #000000",
+								"#15e8ff #ffb115 #ff703f #ff3764 #ff0087",]
 
 		self.pattern_name_list = ["magnetic", "vinyl", "coalesce"]
 		self.special_mode_list = ["on", "off", "retail", "none"]
@@ -83,6 +86,15 @@ class LED_DB_Manager():
 			index = 1
 			for i in i.split(" "):
 				command = """insert into config_table values('ui.led.customize_group.%d.level%d_rgb','%s','#ffffff',%d,0,0);"""%(group_index, index, i, self.color_datatype)
+				self.cursor.execute(command)
+				index = index + 1
+			group_index = group_index + 1
+
+		group_index = 0
+		for i in self.vinyl_color_list:
+			index = 1
+			for i in i.split(" "):
+				command = """insert into config_table values('ui.led.customize_vinyl_group.%d.level%d_rgb','%s','#ffffff',%d,0,0);"""%(group_index, index, i, self.color_datatype)
 				self.cursor.execute(command)
 				index = index + 1
 			group_index = group_index + 1
